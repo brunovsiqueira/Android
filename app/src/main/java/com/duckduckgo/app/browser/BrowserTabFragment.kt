9caaa06a0899aa97c0ecfaa5b02eb27a13b0a5fa
David González <malmstein@gmail.com>
2026-02-25 22:22:41 +0100

    wait until the chat has been loaded to collect the context

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -1951,6 +1951,18 @@ class BrowserTabFragment :
             },
         )
 
+        sharedContextualViewModel.commands
+            .onEach { command ->
+                when (command) {
+                    is DuckChatContextualSharedViewModel.Command.CollectPageContext -> {
+                        logcat { "Duck.ai Contextual: Collect Page Context" }
+                        viewModel.collectPageContext()
+                    }
+
+                    else -> {}
+                }
+            }.launchIn(lifecycleScope)
+
         viewModel.privacyShieldViewState.observe(
             viewLifecycleOwner,
             Observer {
@@ -3431,8 +3443,6 @@ class BrowserTabFragment :
 
         reactToDuckChatContextualSheetResult()
         ensureBrowserIsCompatibleWithContextualSheetState()
-
-        viewModel.collectPageContext()
     }
 
     private fun createNewContextualFragment(tabId: String) {
